bec7c9cc2e29f46775efd7ecd054756235e35ae5
Ayushmanonlycode <ayushman.onlycode69@gmail.com>
2025-05-18 23:32:32 +0530

    Add @vercel/analytics

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 
 import { TempoDevtools } from "tempo-devtools";
 TempoDevtools.init();
@@ -15,6 +16,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <App />
       <SpeedInsights />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>,
 );
